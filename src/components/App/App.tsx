4da2d52db0e0f53adae18962fd79ff7e4ae96285
vartexxx <vartexxx29@yandex.ru>
2023-03-12 11:08:27 +0500

    feat: добавлен API, useEffect

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,19 @@
+import React from 'react';
 import { AppHeader } from '../AppHeader/AppHeader';
 import { BurgerIngredients } from '../BurgerIngredients/BurgerIngredients';
 import { BurgerConstructor } from '../BurgerConstructor/BurgerConstructor';
 import styles from './App.module.scss';
 
+import getApi from '../../utils/api.js';
+
 
 function App() {
+  const [state, setState] = React.useState([])
+  React.useEffect(() => {
+    getApi()
+      .then((data) => setState(data.data))
+      .catch((err) => console.log(err))
+  })
   return (
     <>
       <AppHeader />
