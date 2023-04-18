60d30b20789f2d3f0bbfc4c1fad8e8f8a783787d
vartexxx <vartexxx29@yandex.ru>
2023-04-18 12:25:48 +0500

    fix: установлен frame-motion, uuid для идентификаторов

--- src/components/BurgerConstructor/BurgerConstructor.jsx
+++ src/components/BurgerConstructor/BurgerConstructor.jsx
@@ -5,19 +5,25 @@ import OrderDetails from '../OrderDetails/OrderDetails';
 import styles from './BurgerConstructor.module.scss';
 import { makeOrder } from '../../services/actions/burgerOrderAction';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { ADD, DELETE, SORT } from '../../services/actions/burgerConstructorAction';
+import { v4 as uuidv4 } from 'uuid';
+import { Reorder } from "framer-motion"
 
 const BurgerConstructor = () => {
     const dispatch = useDispatch();
-    const bun = useSelector(
-        (store) => store.burgerConstructorReducer.burgerConstructorBun
-    );
-    const burgerList = useSelector(
-        (store) => store.burgerConstructorReducer.burgerConstructorList
-    );
-    const order = useSelector(
-        (store) => store.burgerOrderReducer.order
-    )
+    // const bun = useSelector(
+    //     (store) => store.burgerConstructorReducer.burgerConstructorBun
+    // );
+    // const burgerList = useSelector(
+    //     (store) => store.burgerConstructorReducer.burgerConstructorList
+    // );
+    const { bun, burgerList } = useSelector((store) => ({
+        bun: store.burgerConstructorReducer.burgerConstructorBun,
+        burgerList: store.burgerConstructorReducer.burgerConstructorList,
+    }))
+    // const order = useSelector(
+    //     (store) => store.burgerOrderReducer.order
+    // )
     const ingredients = useSelector(
         (store) => store.burgerConstructorReducer
     )
@@ -31,6 +37,20 @@ const BurgerConstructor = () => {
         return bun === undefined ? burgerListPrice : burgerListPrice + bunPrice
     }, [bunPrice, burgerListPrice, bun]);
 
+    function onDropHandler(ingredient) {
+        dispatch({ type: ADD_INGREDIENT, id: uuidv4(), payload: ingredient });
+      }
+    
+    const [{ isHover }, dropTarget] = useDrop({
+        accept: "ingredients",
+        drop(ingredient) {
+          onDropHandler(ingredient);
+        },
+        collect: (monitor) => ({
+          isHover: monitor.isOver(),
+        }),
+    });
+
     const [modal, setModal] = useState(false);
     const [orderNumber, setOrderNumber] = useState(undefined);
     const [error, setError] = useState('');
@@ -41,40 +61,43 @@ const BurgerConstructor = () => {
 
     return(
         <section className={`${styles.burger__constructor} ml-4`}>
-            <ul className={styles.burger__container}>
-                <li className='pl-2'>
-                    <ConstructorElement 
-                        type='top'
-                        isLocked={true}
-                        text={`${bun?.name} (верх)`}
-                        price={Number(bun?.price)}
-                        thumbnail={String(bun?.image)}
-                    />
-                </li>
-                <ul className={`${styles.burger__list} pr-2`}>
-                    {burgerList.map((item) => {
-                        return(
-                            <li key={item._id} className={styles.burger__item}>
-                                <DragIcon type="primary" />
-                                <ConstructorElement 
-                                    text={item.name}
-                                    price={item.price}
-                                    thumbnail={item.image}
-                                />
-                            </li>
-                        )
-                    })}
+            <div className={styles.burger__container}>
+                <ul ref={dropTarget} className={isHover ? styles.burger__lists_hover : styles.burger__lists}>
+                    <li className='pl-2'>
+                        <ConstructorElement 
+                            type='top'
+                            isLocked={true}
+                            text={`${bun?.name} (верх)`}
+                            price={Number(bun?.price)}
+                            thumbnail={String(bun?.image)}
+                        />
+                    </li>
+                    <ul className={`${styles.burger__list} pr-2`}>
+                        {burgerList.map((item) => {
+                            return(
+                                <li key={item._id} className={styles.burger__item}>
+                                    <DragIcon type="primary" />
+                                    <ConstructorElement 
+                                        text={item.name}
+                                        price={item.price}
+                                        thumbnail={item.image}
+                                    />
+                                </li>
+                            )
+                        })}
+                    </ul>
+                    <li className='pl-2'>
+                        <ConstructorElement
+                            type='bottom'
+                            isLocked={true}
+                            text={`${bun?.name} (низ)`}
+                            price={Number(bun?.price)}
+                            thumbnail={String(bun?.image)}
+                        />
+                    </li>
                 </ul>
-                <li className='pl-2'>
-                    <ConstructorElement
-                        type='bottom'
-                        isLocked={true}
-                        text={`${bun?.name} (низ)`}
-                        price={Number(bun?.price)}
-                        thumbnail={String(bun?.image)}
-                    />
-                </li>
-            </ul>
+            </div>
+
             <div className={`${styles.burger__order} mt-10 mr-4`}>
                 <div className={styles.burger__price}>
                     <p className="text text_type_digits-medium">{totalPrice}</p>
